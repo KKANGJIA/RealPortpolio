8224451b1e491ca4ecb14a840d3f99571f4411e9
rkdwldk24 <rkdwldk16@gmail.com>
2021-10-02 20:08:40 +0900

    프로젝트 수정

--- resume/main.js
+++ resume/main.js
@@ -1,18 +1,23 @@
 "use strict"
 
 const $$detail = document.querySelectorAll('.show-detail');
-const $imgHome = document.querySelector('.img-home');
+const $imgShopping = document.querySelector('.img-shopping');
 const $imgAT = document.querySelector('.img-aT');
-const $imgToy = document.querySelector('.img-toy');
-const HOME_URL = ['./imgs/project1-1.JPG', './imgs/project1-2.JPG', './imgs/project1-4.JPG'];
+const $imgDiary = document.querySelector('.img-diary');
+const $imgYoutube = document.querySelector('.img-youtube');
 const AT_URL = ['./imgs/project2-1.JPG', './imgs/project2-2.JPG', './imgs/project2-3.JPG'];
-const TOY_URL = ['./imgs/project3-1.JPG'];
+const DIARY_URL = ['./imgs/project3-1.JPG'];
+const SHOPPING_URL = ['./imgs/shoppingList1.jpg', './imgs/shoppingList2.jpg'];
+const YOUTUBE_URL = ['./imgs/youtube1.JPG'];
+// const STUDY_URL = ['./imgs/project2-1.JPG', './imgs/project2-2.JPG', './imgs/project2-3.JPG'];
+// const JOLJACK_URL = ['./imgs/project3-1.JPG'];
+// const MOTION_URL = ['./imgs/project3-1.JPG'];
 const $$dots = document.querySelectorAll('.dot');
 const $aboutMe = document.querySelector('.about-me');
 
 
 
-//각 프로젝트 토글 기능
+// //각 프로젝트 토글 기능
 Array.from($$detail).forEach((v,i) => {
   const detailTag = v.querySelector('.detail');
   v.addEventListener('click', (event) => {
@@ -32,10 +37,11 @@ Array.from($$dots).forEach((v,i) => {
   $$dots[i].addEventListener('click', ()=> {
     const clicks = $$dots[i].querySelectorAll('.click');
     Array.from(clicks).forEach((v,i) => {
-      clicks[i].addEventListener('click', () => {
-        $imgHome.src = HOME_URL[i]; //클릭 버튼에 따라서 이미지 변경
+      clicks[i].addEventListener('click', () => { //클릭 버튼에 따라서 이미지 변경
         $imgAT.src = AT_URL[i];
-        $imgToy.src = TOY_URL[i];
+        $imgDiary.src = DIARY_URL[i];
+        $imgShopping.src = SHOPPING_URL[i];
+        $imgYoutube.src =  YOUTUBE_URL[i];
       })
     })
   })
@@ -44,8 +50,6 @@ Array.from($$dots).forEach((v,i) => {
 
 
 const first_line = '안녕하세요. 저는 강지아입니다. 웹 개발 분야의 프론트엔드 직무를 희망하며 하루하루 꿈을 코딩하고 있습니다.';
-const second_line = "4차산업 시대 속에서 IT의 무한한 '성장 가능성'과 새로운 'IT 트렌드'에 관심이 있습니다.";
-const third_line = '제 목표는 안다고 단언하지않고 내부의 동작원리를 알고 기본에 집중하는 겸손한 개발자가 되는 것 입니다.';
 
 //타이핑 효과 적용
 setTimeout(() => {
@@ -58,16 +62,16 @@ function startEffect(){
       $aboutMe.children[0].append(v);
     }, i * 130)
   });
-  second_line.split('').map((v, i) => {
-    setTimeout(() => {
-      $aboutMe.children[1].append(v);
-    }, (i+65) * 130)
-  });
-  third_line.split('').map((v, i) => {
-    setTimeout(() => {
-      $aboutMe.children[2].append(v);
-    }, (i+125) * 130)
-  });
+  // second_line.split('').map((v, i) => {
+  //   setTimeout(() => {
+  //     $aboutMe.children[1].append(v);
+  //   }, (i+65) * 130)
+  // });
+  // third_line.split('').map((v, i) => {
+  //   setTimeout(() => {
+  //     $aboutMe.children[2].append(v);
+  //   }, (i+125) * 130)
+  // });
 }
 
 
